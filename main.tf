df36bca2ca0c70e610e11e9b56ec0b7833efca42
Flamarion Jorge <jorge.flamarion@gmail.com>
2021-12-18 10:20:19 +0100

    Adding TF and provider verrsion constraints

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "aws_vpc" "vpc" {
   cidr_block           = var.cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
